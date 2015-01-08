7ad44b8ed79cf8e91798850d2bf9595193e4fad3
camshaft <bytheway.cameron@gmail.com>
2015-01-07 22:30:33 -0700

    add client response codes

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ var msgpack = require('msgpack');
 
 var TYPES = {
   call: 0,
-  cast: 1
+  cast: 1,
+  response: 0,
+  error: 1
 };
 
-exports.encode = function(msgid, type, mod, fun, args) {
+exports.encode = function(type, msgid, mod, fun, args) {
+  var typenum = TYPES[type];
+  if (typeof typenum === 'undefined') throw new Error('unrecognized type ' + type);
+  if (arguments.length === 3) return msgpack.pack([TYPES[type], msgid, mod]);
   return msgpack.pack([TYPES[type], msgid, mod, fun, args]);
 };
 
